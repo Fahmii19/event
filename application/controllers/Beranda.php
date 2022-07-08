cef4ffbb8f8f28650e0bd18d7cfef03385615029
Candrawjy <candraw71@gmail.com>
2022-07-08 16:02:30 +0700

    Can: edit beranda

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -3,10 +3,26 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Beranda extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("datakegiatan");
+
+        // if ($this->session->userdata('role') == NULL) {
+        //     echo "<script> alert('Anda belum login, silahkan login terlebih dahulu!');
+        //     history.go(-1); </script>";
+        // } else if ($this->session->userdata('role') == "public") {
+        //     echo "<script> alert('Anda sedang login sebagai " .$this->session->userdata('role'). ", silahkan logout terlebih dahulu!');
+        //     history.go(-1); </script>";
+        // }
+    }
+
     public function index()
     {
+        $data['kegiatan'] = $this->datakegiatan->getAll();
+
         $this->load->view('partial_user/header');
-        $this->load->view('beranda');
+        $this->load->view('beranda', $data);
         $this->load->view('partial_user/footer');
     }
 
